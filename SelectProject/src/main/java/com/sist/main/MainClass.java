29c78b350ccc194b8ebd1ae3dca85c3c3fc1caa2
user <user@DESKTOP-COJ9858>
2022-12-08 15:45:18 +0900

    1111

---
+++ SelectProject/src/main/java/com/sist/main/MainClass.java
@@ -0,0 +1,24 @@
+package com.sist.main;
+import java.util.*;
+import com.sist.dao.*;
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // 오라클 연결 
+		EmpDAO dao=EmpDAO.newInstance();// 싱글턴 객체 생성 
+		ArrayList<EmpVO> list=dao.empListData();
+		for(EmpVO vo:list)
+		{
+			System.out.println(vo.getEmpno()+" "
+					+vo.getEname()+" "
+					+vo.getJob()+" "
+					+vo.getHiredate().toString()+" "
+					+vo.getSal()+" "
+					+vo.getDvo().getDname()+" "
+					+vo.getDvo().getLoc()+" "
+					+vo.getSvo().getGrade());
+		}
+	}
+
+}
